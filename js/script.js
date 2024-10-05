4cc0b42a034a725c3e399bba8952287802ca6dad
Aldy Wirandhoko <aldywirandhoko@gmail.com>
2024-10-06 01:41:41 +0800

    Menampilkan search form

--- js/script.js
+++ js/script.js
@@ -4,12 +4,27 @@ const navbarNav = document.querySelector(".navbar-nav");
 
 hamburgerMenu.addEventListener("click", function (e) {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 });
 
-// Klik di luar sidebar menghilangkan nav
+// toggle search form active
+const search = document.querySelector("#search-btn");
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+search.addEventListener("click", function (e) {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+});
+
+// Klik di luar elemen
 
 document.addEventListener("click", function (e) {
   if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!search.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
